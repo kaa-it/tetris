e669dc66a1d47d4986c43bcf694b888bd2170ba4
Andrey Kruglov <kruglov.it@gmail.com>
2021-01-11 13:31:33 +0300

    Initial

---
+++ app/src/main/java/ru/tvhelp/tetris/MainActivity.kt
@@ -0,0 +1,14 @@
+package ru.tvhelp.tetris
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+    }
+}
